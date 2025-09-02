ae542a685bfca8d0a2ef4e66c58f1f5d9568e44f
shubh <shubhjain798@gmailcom>
2025-09-02 15:43:19 +0530

    coppied from mainbranch to features

--- lib/features/search/search_page.dart
+++ lib/features/search/search_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../core/share_prederence/share_preferences.dart';
 import '../dashboard/presentation/bloc/dashboard_cubit.dart';
 
